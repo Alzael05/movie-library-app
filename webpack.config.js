9aa541e7184bbcf8f819647445792e7c68300524
Alzael05 <17470186+Alzael05@users.noreply.github.com>
2017-09-25 16:57:27 +0800

    This is my first commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,15 @@ const path = require('path');
 
 
 module.exports = function (env) {
+
+	const prefix = env.production ? 'min' : 'build';
 	//define entry point
 	entry: path.resolve( __dirname, '/assets/js/custom/announcements.js' ),
 
 	//define output point
 	output: {
-		path: path.resolve( __dirname, '/assets/bundle' ),
-		filename: 'announcement-bundle.js'
+		filename: `[name].${prefix}.js`,
+		path: path.resolve(__dirname, 'public/assets/js'),
+		publicPath: '/assets/js/'
 	}
 }
